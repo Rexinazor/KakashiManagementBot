0a5dc97bda57eff0d00b27430b0150e660653929
Rexinazor <85327329+Rexinazor@users.noreply.github.com>
2021-06-13 15:15:33 +0530

    Update anime.py

--- KakashiManagementBot/modules/anime.py
+++ KakashiManagementBot/modules/anime.py
@@ -5,8 +5,8 @@ import textwrap
 import bs4
 import jikanpy
 import requests
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from KakashiManagementBot import dispatcher
+from KakashiManagementBot.modules.disable import DisableAbleCommandHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update, Message
 from telegram.ext import CallbackContext, run_async
 
