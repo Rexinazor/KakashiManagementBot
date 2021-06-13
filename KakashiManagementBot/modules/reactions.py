495e5aee3993f1e4e007b5d9d32dc648eba55ed7
Rexinazor <85327329+Rexinazor@users.noreply.github.com>
2021-06-13 21:50:15 +0530

    Update reactions.py

--- KakashiManagementBot/modules/reactions.py
+++ KakashiManagementBot/modules/reactions.py
@@ -1,7 +1,7 @@
 import random
 
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from KakashiManagementBot import dispatcher
+from KakashiManagementBot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
