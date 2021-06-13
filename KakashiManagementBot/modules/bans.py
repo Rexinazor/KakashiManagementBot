a6542882519f0b1166fefe08b5eb2e5445a3814d
Rexinazor <85327329+Rexinazor@users.noreply.github.com>
2021-06-13 15:36:41 +0530

    Update bans.py

--- KakashiManagementBot/modules/bans.py
+++ KakashiManagementBot/modules/bans.py
@@ -5,7 +5,7 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from SaitamaRobot import (
+from KakashiManagementBot import (
     DEV_USERS,
     LOGGER,
     OWNER_ID,
@@ -15,8 +15,8 @@ from SaitamaRobot import (
     WOLVES,
     dispatcher,
 )
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import (
+from KakashiManagementBot.modules.disable import DisableAbleCommandHandler
+from KakashiManagementBot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     connection_status,
@@ -27,9 +27,9 @@ from SaitamaRobot.modules.helper_funcs.chat_status import (
     user_can_ban,
     can_delete,
 )
-from SaitamaRobot.modules.helper_funcs.extraction import extract_user_and_text
-from SaitamaRobot.modules.helper_funcs.string_handling import extract_time
-from SaitamaRobot.modules.log_channel import gloggable, loggable
+from KakashiManagementBot.modules.helper_funcs.extraction import extract_user_and_text
+from KakashiManagementBot.modules.helper_funcs.string_handling import extract_time
+from KakashiManagementBot.modules.log_channel import gloggable, loggable
 
 
 @run_async
