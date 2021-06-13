6cda2179e55de19e0bc045196eaf99f50fa83486
Rexinazor <85327329+Rexinazor@users.noreply.github.com>
2021-06-13 21:55:21 +0530

    Update sed.py

--- KakashiManagementBot/modules/sed.py
+++ KakashiManagementBot/modules/sed.py
@@ -2,9 +2,9 @@ import sre_constants
 
 import regex
 import telegram
-from SaitamaRobot import LOGGER, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleMessageHandler
-from SaitamaRobot.modules.helper_funcs.regex_helper import infinite_loop_check
+from KakashiManagementBot import LOGGER, dispatcher
+from KakashiManagementBot.modules.disable import DisableAbleMessageHandler
+from KakashiManagementBot.modules.helper_funcs.regex_helper import infinite_loop_check
 from telegram import Update
 from telegram.ext import CallbackContext, Filters, run_async
 
