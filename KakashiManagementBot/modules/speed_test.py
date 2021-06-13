a843facd817af36e87d6a66bfe6474736e3c50e8
Rexinazor <85327329+Rexinazor@users.noreply.github.com>
2021-06-13 22:05:28 +0530

    Update speed_test.py

--- KakashiManagementBot/modules/speed_test.py
+++ KakashiManagementBot/modules/speed_test.py
@@ -1,7 +1,7 @@
 import speedtest
-from SaitamaRobot import DEV_USERS, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import dev_plus
+from KakashiManagementBot import DEV_USERS, dispatcher
+from KakashiManagementBot.modules.disable import DisableAbleCommandHandler
+from KakashiManagementBot.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
 
