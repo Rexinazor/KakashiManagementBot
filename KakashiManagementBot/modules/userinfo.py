67460490bc96515227178576dced5de639c00031
Rexinazor <85327329+Rexinazor@users.noreply.github.com>
2021-06-13 22:07:53 +0530

    Update userinfo.py

--- KakashiManagementBot/modules/userinfo.py
+++ KakashiManagementBot/modules/userinfo.py
@@ -13,7 +13,7 @@ from telegram.ext.dispatcher import run_async
 from telegram.error import BadRequest
 from telegram.utils.helpers import escape_markdown, mention_html
 
-from SaitamaRobot import (
+from KakashiManagementBot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -24,15 +24,15 @@ from SaitamaRobot import (
     dispatcher,
     sw,
 )
-from SaitamaRobot.__main__ import STATS, TOKEN, USER_INFO
-import SaitamaRobot.modules.sql.userinfo_sql as sql
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.sql.global_bans_sql import is_user_gbanned
-from SaitamaRobot.modules.sql.afk_sql import is_afk, check_afk_status
-from SaitamaRobot.modules.sql.users_sql import get_user_num_chats
-from SaitamaRobot.modules.helper_funcs.chat_status import sudo_plus
-from SaitamaRobot.modules.helper_funcs.extraction import extract_user
-from SaitamaRobot import telethn as SaitamaTelethonClient
+from KakashiManagementBot.__main__ import STATS, TOKEN, USER_INFO
+import KakashiManagementBot.modules.sql.userinfo_sql as sql
+from KakashiManagementBot.modules.disable import DisableAbleCommandHandler
+from KakashiManagementBot.modules.sql.global_bans_sql import is_user_gbanned
+from KakashiManagementBot.modules.sql.afk_sql import is_afk, check_afk_status
+from KakashiManagementBot.modules.sql.users_sql import get_user_num_chats
+from KakashiManagementBot.modules.helper_funcs.chat_status import sudo_plus
+from KakashiManagementBot.modules.helper_funcs.extraction import extract_user
+from KakashiManagementBot import telethn as SaitamaTelethonClient
 
 
 def no_by_per(totalhp, percentage):
