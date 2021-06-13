fbc97b959956140ac5c4752aa6b48bfe6d585e09
Rexinazor <85327329+Rexinazor@users.noreply.github.com>
2021-06-13 21:37:51 +0530

    Update misc.py

--- KakashiManagementBot/modules/misc.py
+++ KakashiManagementBot/modules/misc.py
@@ -1,6 +1,6 @@
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot import dispatcher
+from KakashiManagementBot.modules.helper_funcs.chat_status import user_admin
+from KakashiManagementBot.modules.disable import DisableAbleCommandHandler
+from KakashiManagementBot import dispatcher
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram import ParseMode, Update
