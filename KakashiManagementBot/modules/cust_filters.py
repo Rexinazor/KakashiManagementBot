bf3e9e218baa70357721deb4583d4fdb652a64d3
Rexinazor <85327329+Rexinazor@users.noreply.github.com>
2021-06-13 17:41:12 +0530

    Update cust_filters.py

--- KakashiManagementBot/modules/cust_filters.py
+++ KakashiManagementBot/modules/cust_filters.py
@@ -15,25 +15,25 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import mention_html, escape_markdown
 
-from SaitamaRobot import dispatcher, LOGGER, DRAGONS
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.handlers import MessageHandlerChecker
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
-from SaitamaRobot.modules.helper_funcs.extraction import extract_text
-from SaitamaRobot.modules.helper_funcs.filters import CustomFilters
-from SaitamaRobot.modules.helper_funcs.misc import build_keyboard_parser
-from SaitamaRobot.modules.helper_funcs.msg_types import get_filter_type
-from SaitamaRobot.modules.helper_funcs.string_handling import (
+from KakashiManagementBot import dispatcher, LOGGER, DRAGONS
+from KakashiManagementBot.modules.disable import DisableAbleCommandHandler
+from KakashiManagementBot.modules.helper_funcs.handlers import MessageHandlerChecker
+from KakashiManagementBot.modules.helper_funcs.chat_status import user_admin
+from KakashiManagementBot.modules.helper_funcs.extraction import extract_text
+from KakashiManagementBot.modules.helper_funcs.filters import CustomFilters
+from KakashiManagementBot.modules.helper_funcs.misc import build_keyboard_parser
+from KakashiManagementBot.modules.helper_funcs.msg_types import get_filter_type
+from KakashiManagementBot.modules.helper_funcs.string_handling import (
     split_quotes,
     button_markdown_parser,
     escape_invalid_curly_brackets,
     markdown_to_html,
 )
-from SaitamaRobot.modules.sql import cust_filters_sql as sql
+from KakashiManagementBot.modules.sql import cust_filters_sql as sql
 
-from SaitamaRobot.modules.connection import connected
+from KakashiManagementBot.modules.connection import connected
 
-from SaitamaRobot.modules.helper_funcs.alternate import send_message, typing_action
+from KakashiManagementBot.modules.helper_funcs.alternate import send_message, typing_action
 
 HANDLER_GROUP = 10
 
