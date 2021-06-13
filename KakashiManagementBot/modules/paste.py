39fd56be0e3de3d54e880061f3989e1be42e69cc
Rexinazor <85327329+Rexinazor@users.noreply.github.com>
2021-06-13 21:47:28 +0530

    Update paste.py

--- KakashiManagementBot/modules/paste.py
+++ KakashiManagementBot/modules/paste.py
@@ -1,6 +1,6 @@
 import requests
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from KakashiManagementBot import dispatcher
+from KakashiManagementBot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
