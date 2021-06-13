ef920d80a95a58abd80475b0a3e4725939219766
Rexinazor <85327329+Rexinazor@users.noreply.github.com>
2021-06-13 15:28:09 +0530

    Update approve.py

--- KakashiManagementBot/modules/approve.py
+++ KakashiManagementBot/modules/approve.py
@@ -1,11 +1,11 @@
 import html
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot import dispatcher, DRAGONS
-from SaitamaRobot.modules.helper_funcs.extraction import extract_user
+from KakashiManagementBot.modules.disable import DisableAbleCommandHandler
+from KakashiManagementBot import dispatcher, DRAGONS
+from KakashiManagementBot.modules.helper_funcs.extraction import extract_user
 from telegram.ext import CallbackContext, run_async, CallbackQueryHandler
-import SaitamaRobot.modules.sql.approve_sql as sql
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
-from SaitamaRobot.modules.log_channel import loggable
+import KakashiManagementBot.modules.sql.approve_sql as sql
+from KakashiManagementBot.modules.helper_funcs.chat_status import user_admin
+from KakashiManagementBot.modules.log_channel import loggable
 from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Update
 from telegram.utils.helpers import mention_html
 from telegram.error import BadRequest
