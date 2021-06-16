061774ecfe13fbd6dd1199f289aebbf40e1f9866
Rexinazor <85327329+Rexinazor@users.noreply.github.com>
2021-06-16 21:52:43 +0530

    Update approve_sql.py

--- KakashiManagementBot/modules/sql/approve_sql.py
+++ KakashiManagementBot/modules/sql/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, Integer
 
-from SaitamaRobot.modules.sql import BASE, SESSION
+from KakashiManagementBot.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):
